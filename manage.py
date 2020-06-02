8897db0b0760fd18d4ce01dda603959f120cfee7
liuyujiangx <1914571065@qq.com>
2020-06-02 20:02:27 +0800

    first

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 from app import app
-
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 if __name__ == "__main__":
-    app.run()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5566)  # flask默认的端口
+    IOLoop.instance().start()
