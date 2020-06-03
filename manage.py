1a510d0dc09307915de89d13463bf12ce971c2a5
liuyujiangx <1914571065@qq.com>
2020-06-03 13:56:44 +0800

    first

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@ from app import app
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, options
+import logging
 
 if __name__ == "__main__":
-    # http_server = HTTPServer(WSGIContainer(app))
-    # http_server.listen(5566)  # flask默认的端口
-    # IOLoop.instance().start()
-    app.run(port=5566,host='0.0.0.0')
+    #app.run(port=5566,host='0.0.0.0',threaded=True)
+    options.parse_command_line()
+    logging.info('[UOP] UOP is starting...')
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5566)
+    IOLoop.instance().start()
